e489556dfad168bf11aecf28d649665f66159937
Hưởng Nguyễn Văn <huongnguyenvan@Huongs-MacBook-Air.local>
2020-11-15 10:53:50 +0700

    update database

---
+++ dulieusql/data.sql
@@ -0,0 +1,92 @@
+-- MySQL dump 10.13  Distrib 8.0.22, for macos10.15 (x86_64)
+--
+-- Host: 127.0.0.1    Database: save_animals
+-- ------------------------------------------------------
+-- Server version	8.0.22
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `Activity`
+--
+
+LOCK TABLES `Activity` WRITE;
+/*!40000 ALTER TABLE `Activity` DISABLE KEYS */;
+INSERT INTO `Activity` VALUES (1,'Cynthia Whitney','<p><span style=\"color: rgb(33, 37, 41); font-family: Lora, \"Times New Roman\", serif; font-size: 20px;\">Science cuts two ways, of course; its products can be used for both good and evil. But there\'s no turning back from science. The early warnings about technological dangers also come from science.</span><br></p>',10000,'Placeat quia illo d',1),(2,'Brock Potts','<div class=\"sqs-block html-block sqs-block-html\" data-block-type=\"2\" id=\"block-21fd20a7982c8cee4be2\" style=\"position: relative; height: auto; padding-right: 17px; padding-bottom: 17px; padding-left: 17px; clear: none; box-sizing: border-box; width: 649.359px; margin-right: auto; margin-left: auto; color: rgba(29, 29, 29, 0.7); font-family: Roboto; font-size: 14px; letter-spacing: 0.84px;\"><div class=\"sqs-block-content\" style=\"outline: none;\"><p class=\"\" style=\"hyphens: manual !important; overflow-wrap: break-word; text-align: center; white-space: pre-wrap;\">Do you have a special place in your heart for elephants? Maybe rhinos are your favorite animal. Perhaps you\'d like to donate towards our most needed projects.&nbsp;We think it\'s important for you to choose how your donation is used.&nbsp;</p><p class=\"\" style=\"hyphens: manual !important; overflow-wrap: break-word; text-align: center; white-space: pre-wrap;\">&nbsp;</p><h3 style=\"text-rendering: optimizelegibility; font-family: brandon-grotesque; font-size: 20px; letter-spacing: 0.06em; line-height: 1.2em; color: rgb(29, 29, 29); margin-bottom: 0px; text-align: center; white-space: pre-wrap; hyphens: manual !important;\">MOST NEEDED</h3></div></div><div class=\"row sqs-row\" id=\"yui_3_17_2_1_1605346682586_71\" style=\"color: rgba(29, 29, 29, 0.7); font-family: Roboto; font-size: 14px; letter-spacing: 0.84px; width: auto !important;\"><div class=\"col sqs-col-5 span-5\" style=\"float: left; width: 541.25px;\"><div class=\"sqs-block spacer-block sqs-block-spacer sized vsize-1\" data-block-type=\"21\" id=\"block-yui_3_17_2_18_1494350135954_15302\" style=\"position: relative; height: auto; padding: 17px; clear: both;\"><div class=\"sqs-block-content\" style=\"visibility: hidden; height: 34px; overflow: hidden;\"></div></div></div><div class=\"col sqs-col-2 span-2\" id=\"yui_3_17_2_1_1605346682586_70\" style=\"float: left; width: 216.5px;\"><div class=\"sqs-block image-block sqs-block-image sqs-text-ready\" data-block-type=\"5\" id=\"block-yui_3_17_2_18_1494350135954_13930\" style=\"position: relative; height: auto; padding: 17px; clear: both;\"><div class=\"sqs-block-content\" id=\"yui_3_17_2_1_1605346682586_69\"><div class=\"image-block-outer-wrapper\r\n          layout-caption-below\r\n          design-layout-inline\r\n          combination-animation-none\r\n          individual-animation-none\r\n          individual-text-animation-none\r\n         sqs-narrow-width\" data-test=\"image-block-inline-outer-wrapper\" id=\"yui_3_17_2_1_1605346682586_68\"><figure class=\"\r\n              sqs-block-image-figure\r\n              intrinsic\r\n            \" id=\"yui_3_17_2_1_1605346682586_67\" style=\"margin: auto; max-width: 367px;\"><a class=\"\r\n                sqs-block-image-link\r\n                \r\n          \r\n        \r\n              \" href=\"https://www.wildtomorrowfund.org/donate-most-needed\" id=\"yui_3_17_2_1_1605346682586_66\" style=\"color: inherit; outline: 0px; display: block;\"><div class=\"\r\n                image-block-wrapper\r\n                \r\n          \r\n        \r\n                has-aspect-ratio\r\n              \" data-animation-role=\"image\" id=\"yui_3_17_2_1_1605346682586_65\" style=\"line-height: 0; text-align: center; position: relative; overflow: hidden; padding-bottom: 181.5px;\"><img class=\"thumb-image loaded\" data-src=\"https://images.squarespace-cdn.com/content/v1/58f4c8cad1758ec58b2bcc52/1496776752451-VB6RUHUUSUF7TJMP39DG/ke17ZwdGBToddI8pDm48kKDzY87COcvujd3ba_6KkuFZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVHTVGgi5M9BOMS6TnrrEDOlZ3koG8kVAo_Cujqyka-Hkr8n7QeS9uE4HniGx411cDA/image-asset.jpeg\" data-image=\"https://images.squarespace-cdn.com/content/v1/58f4c8cad1758ec58b2bcc52/1496776752451-VB6RUHUUSUF7TJMP39DG/ke17ZwdGBToddI8pDm48kKDzY87COcvujd3ba_6KkuFZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVHTVGgi5M9BOMS6TnrrEDOlZ3koG8kVAo_Cujqyka-Hkr8n7QeS9uE4HniGx411cDA/image-asset.jpeg\" data-image-dimensions=\"367x365\" data-image-focal-point=\"0.5,0.5\" alt=\"\" data-load=\"false\" data-image-id=\"59370030440243161ff5f287\" data-type=\"image\" data-image-resolution=\"300w\" src=\"https://images.squarespace-cdn.com/content/v1/58f4c8cad1758ec58b2bcc52/1496776752451-VB6RUHUUSUF7TJMP39DG/ke17ZwdGBToddI8pDm48kKDzY87COcvujd3ba_6KkuFZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVHTVGgi5M9BOMS6TnrrEDOlZ3koG8kVAo_Cujqyka-Hkr8n7QeS9uE4HniGx411cDA/image-asset.jpeg?format=300w\" style=\"border: 0px; display: block; max-width: none; position: absolute; top: 0px; left: 0px; width: 182.5px; height: 181.5px;\"></div></a></figure></div></div></div></div></div>',12,'Voluptates eu ut fug',2132);
+/*!40000 ALTER TABLE `Activity` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `Article`
+--
+
+LOCK TABLES `Article` WRITE;
+/*!40000 ALTER TABLE `Article` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Article` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `donor`
+--
+
+LOCK TABLES `donor` WRITE;
+/*!40000 ALTER TABLE `donor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `donor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `employee`
+--
+
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `provinces`
+--
+
+LOCK TABLES `provinces` WRITE;
+/*!40000 ALTER TABLE `provinces` DISABLE KEYS */;
+/*!40000 ALTER TABLE `provinces` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `reports`
+--
+
+LOCK TABLES `reports` WRITE;
+/*!40000 ALTER TABLE `reports` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reports` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `User`
+--
+
+LOCK TABLES `User` WRITE;
+/*!40000 ALTER TABLE `User` DISABLE KEYS */;
+INSERT INTO `User` VALUES (1,'admin','1',1,1,'1');
+/*!40000 ALTER TABLE `User` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-11-15 10:51:01
---
+++ dulieusql/structure.sql
@@ -0,0 +1,165 @@
+-- MySQL dump 10.13  Distrib 8.0.22, for macos10.15 (x86_64)
+--
+-- Host: 127.0.0.1    Database: save_animals
+-- ------------------------------------------------------
+-- Server version	8.0.22
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Activity`
+--
+
+DROP TABLE IF EXISTS `Activity`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Activity` (
+  `activityID` int NOT NULL AUTO_INCREMENT,
+  `activityName` varchar(45) NOT NULL,
+  `activityDescription` varchar(10000) NOT NULL,
+  `activityMember` int NOT NULL,
+  `activityTarget` varchar(45) NOT NULL,
+  `sponsorID` int NOT NULL,
+  PRIMARY KEY (`activityID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Article`
+--
+
+DROP TABLE IF EXISTS `Article`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Article` (
+  `articleID` int NOT NULL AUTO_INCREMENT,
+  `articleImage` varchar(45) NOT NULL,
+  `articleCreatedDate` date NOT NULL,
+  `articleContent` text NOT NULL,
+  `articleTitle` varchar(500) NOT NULL,
+  `articleAuthor` varchar(45) NOT NULL,
+  `articleReference` text NOT NULL,
+  `employeeID` int NOT NULL,
+  PRIMARY KEY (`articleID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `donor`
+--
+
+DROP TABLE IF EXISTS `donor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `donor` (
+  `donorID` int NOT NULL AUTO_INCREMENT,
+  `donorName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `donorSurrogate` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `donorAddress` varchar(225) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `donorEmail` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `donorPhone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `donorMoney` double NOT NULL,
+  `donorTotalMoney` double NOT NULL,
+  `donorImage` varchar(225) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`donorID`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `employee`
+--
+
+DROP TABLE IF EXISTS `employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employee` (
+  `employeeID` int NOT NULL AUTO_INCREMENT,
+  `employeeName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `employeePosition` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `employeeGroup` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `employeeDescription` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `employeeDOB` date NOT NULL,
+  `employeeDegree` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `employeeEmail` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `employeePhone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `employeeAddress` varchar(225) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `employeeCMND` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `employeeProvinceCode` int NOT NULL,
+  `employeeImage` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `employeeGender` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `employeeJoinDate` date NOT NULL,
+  PRIMARY KEY (`employeeID`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `provinces`
+--
+
+DROP TABLE IF EXISTS `provinces`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `provinces` (
+  `provinceID` int NOT NULL AUTO_INCREMENT,
+  `provinceName` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `provinceCode` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`provinceID`)
+) ENGINE=InnoDB AUTO_INCREMENT=72 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `reports`
+--
+
+DROP TABLE IF EXISTS `reports`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reports` (
+  `reportID` int NOT NULL AUTO_INCREMENT,
+  `reportAddress` varchar(225) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `reportDescription` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `reportPersonName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `reportEmail` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `reportPhone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `reportStatus` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`reportID`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `User`
+--
+
+DROP TABLE IF EXISTS `User`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `User` (
+  `userID` int NOT NULL AUTO_INCREMENT,
+  `userUsername` varchar(45) NOT NULL,
+  `userPassword` varchar(45) NOT NULL,
+  `userAdmin` tinyint NOT NULL,
+  `userActive` tinyint NOT NULL,
+  `employeeID` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`userID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-11-15 10:50:28
